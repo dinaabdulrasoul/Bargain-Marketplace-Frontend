79345a991e9242fb0307d016e72f214557b50d87
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-11-29 17:05:29 +0200

    seller info

--- Products Page/src/components/Products/Products.jsx
+++ Products Page/src/components/Products/Products.jsx
@@ -4,11 +4,11 @@ import Product from './Product/Product';
 import useStyles from './styles';
 
 const mock_products = [
-    {id: 1, name: 'Keyboard', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 2, name: 'Dell Latitude 3310 Laptop', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445'},
-    {id: 3, name: 'Apple Macbook', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 4, name: 'Gaming Mouse', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 5, name: 'iPhone XR', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
+    {id: 1, name: 'Keyboard', seller_name: 'x store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
+    {id: 2, name: 'Dell Laptop', seller_name: 'y store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445'},
+    {id: 3, name: 'Apple Macbook', seller_name: 'z store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
+    {id: 4, name: 'Gaming Mouse', seller_name: 'amazon', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
+    {id: 5, name: 'iPhone XR', seller_name: 'souq', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
     {id: 6, name: 'Addidas Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
     {id: 7, name: 'Nike Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
     {id: 8, name: 'Addias Backbag', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' }
