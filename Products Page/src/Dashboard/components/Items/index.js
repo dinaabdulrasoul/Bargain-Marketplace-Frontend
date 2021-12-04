7fab1ea1d27a120f167f5a2e38d43370c9fc7018
atout811 <dmcgfx811@gmail.com>
2021-12-04 22:18:36 +0200

    create Items Table functionality

--- Products Page/src/Dashboard/components/Items/index.js
+++ Products Page/src/Dashboard/components/Items/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "Quantity",
-    headerName: "First name",
+    field: "ItemName",
+    headerName: "Item Name",
     width: 150,
     editable: true,
   },
   {
-    field: "ItemName",
-    headerName: "Last name",
+    field: "Quantity",
+    headerName: "Quantity",
     width: 150,
     editable: true,
   },
@@ -25,19 +25,33 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, ItemName: "Snow", Quantity: "Jon", Price: 35 },
-  { id: 2, ItemName: "Lannister", Quantity: "Cersei", Price: 42 },
-  { id: 3, ItemName: "Lannister", Quantity: "Jaime", Price: 45 },
-  { id: 4, ItemName: "Stark", Quantity: "Arya", Price: 16 },
-  { id: 5, ItemName: "Targaryen", Quantity: "Daenerys", Price: null },
+  { id: 1, ItemName: "Snow", Quantity: "15", Price: 35 },
+  { id: 2, ItemName: "Lannister", Quantity: "10", Price: 42 },
+  { id: 3, ItemName: "Lannister", Quantity: "2", Price: 45 },
+  { id: 4, ItemName: "Stark", Quantity: "4", Price: 16 },
+  { id: 5, ItemName: "Targaryen", Quantity: "165", Price: null },
   { id: 6, ItemName: "Melisandre", Quantity: null, Price: 150 },
-  { id: 7, ItemName: "Clifford", Quantity: "Ferrara", Price: 44 },
-  { id: 8, ItemName: "Frances", Quantity: "Rossini", Price: 36 },
-  { id: 9, ItemName: "Roxie", Quantity: "Harvey", Price: 65 },
+  { id: 7, ItemName: "Clifford", Quantity: "1", Price: 44 },
+  { id: 8, ItemName: "Frances", Quantity: "30", Price: 36 },
+  { id: 9, ItemName: "Roxie", Quantity: "8", Price: 65 },
 ];
 
 export default function Items() {
   const [selected, setSelected] = useState([]);
+  const [editRowsModel, setEditRowsModel] = useState({});
+
+  const handleEditRowsModelChange = React.useCallback((model) => {
+    setEditRowsModel(model);
+  }, []);
+
+  const deleteItem = (selected) => {
+    //here we will send request to delete this items from database
+  };
+
+  useEffect(() => {
+    //fetching data to show in rows
+    return () => {};
+  }, []);
 
   return (
     <div style={{ height: 400, width: "100%" }}>
@@ -52,6 +66,8 @@ export default function Items() {
           const selectedIDs = new Set(ids);
           setSelected(selectedIDs);
         }}
+        editRowsModel={editRowsModel}
+        onEditRowsModelChange={handleEditRowsModelChange}
       />
     </div>
   );
