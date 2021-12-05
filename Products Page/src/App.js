3ad5257fa7eb4acef0f3bad6c175e6ef2830e0ea
Dina Abdul-Rasoul <dinaabdelrasoul@hotmail.com>
2021-12-05 20:29:21 +0200

    fix routes

--- Products Page/src/App.js
+++ Products Page/src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import {Products, Navbar, Cart, SingleProduct} from './components';
 import Auth from './components/Auth/Auth';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import axios from 'axios';
+
 
 const App = () => {
     {/*const [products, setProducts] = useState([]);
@@ -29,18 +31,15 @@ useEffect(()=> {
 
 // Mock products for design only
 const mock_products = [
-    {id: 1, name: 'Keyboard', seller_name: 'x store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 2, name: 'Dell Laptop', seller_name: 'y store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445'},
-    {id: 3, name: 'Apple Macbook', seller_name: 'z store', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 4, name: 'Gaming Mouse', seller_name: 'amazon', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 5, name: 'iPhone XR', seller_name: 'souq', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 6, name: 'Addidas Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 7, name: 'Nike Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' },
-    {id: 8, name: 'Addias Backbag', seller_name: 'x', description: 'Magic iPad Keyboard', price:'300 EGP', image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' }
+    {id: 1, name: 'Apple Macbook', seller_name: 'z store', description: 'Magic iPad Keyboard', price: 300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0},
+    {id: 2, name: 'Dell Laptop', seller_name: 'y store', description: 'Magic iPad Keyboard', price: 300, image: 'https://media.btech.com/media/catalog/product/cache/93efdb2b7ce46b07de94991cbebe71bf/d/e/dell-inspiron-5593-laptop_1_2.jpg',quantity: 0},
+    {id: 3, name: 'Keyboard', seller_name: 'x store', description: 'Magic iPad Keyboard', price: 300, image: 'https://cdn.stamped.io/uploads/productImages/194093_6601656303789.jpg' ,quantity: 0},
+    {id: 4, name: 'Gaming Mouse', seller_name: 'amazon', description: 'Magic iPad Keyboard', price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0},
+    {id: 5, name: 'iPhone XR', seller_name: 'souq', description: 'Magic iPad Keyboard', price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0},
+    {id: 6, name: 'Addidas Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0},
+    {id: 7, name: 'Nike Running Shoes', seller_name: 'x', description: 'Magic iPad Keyboard', price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0},
+    {id: 8, name: 'Addias Backbag', seller_name: 'x', description: 'Magic iPad Keyboard', price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' ,quantity: 0}
 ];
-const item = {id: 1, name: 'Keyboard', seller_name: 'x store', description: 'sDKJnkjdsakljsdmjdslkdslkdlkdsklfdmdkmlfkmckmdxm,fkmd,mm,fmsdkjfmdlkfklsmflksmklfm',
- price:300, image: 'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111160445' 
-,quantity: 0};
     return (
         <Router>
         <div>
@@ -51,8 +50,8 @@ const item = {id: 1, name: 'Keyboard', seller_name: 'x store', description: 'sDK
                 </Route>
                 <Route exact path = "/cart" element = {<Cart cart={{line_items: []}} /> }>
                     </Route>
-                {mock_products.map((product)=>(
-                    <Route exact path = {`products/${product.id}`} element = {<SingleProduct product = {item}/>}>
+                {mock_products.map((p)=>(
+                    <Route exact path = {`products/${p.id}`} element = {<SingleProduct product = {p}/>}>
                         </Route>
                 ))}
             </Routes>
