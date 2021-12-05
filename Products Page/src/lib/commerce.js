f584bde3c67d8df5328e5e7531eb85c1f07a540f
Dina Abdul-Rasoul <dinaabdelrasoul@hotmail.com>
2021-12-05 16:17:17 +0200

    Delete commerce.js

--- Products Page/src/lib/commerce.js
+++
@@ -1,4 +0,0 @@
-import Commerce from '@chec/commerce.js';
-
-//export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
-export const commerce = new Commerce('pk_36504ae4bc07d55c25813e665b2461a1a48be64bc2a76', true);
