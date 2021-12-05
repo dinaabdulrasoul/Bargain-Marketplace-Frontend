328b5ae781747c153229d3602f91ef2ac031144b
Dina Abdul-Rasoul <dinaabdelrasoul@hotmail.com>
2021-12-05 16:30:43 +0200

    increase/decrease  product quantity

--- Products Page/src/components/Cart/CartItem/CartItem.jsx
+++ Products Page/src/components/Cart/CartItem/CartItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core';
+import {Typography, Button, Card, CardActions, CardContent, CardMedia, ListItemIcon} from '@material-ui/core';
 
 import useStyles from './styles';
+import {useState} from 'react';
 
 const CartItem = ({item}) => {
     const classes = useStyles();
+    const [count, setCount] = useState(item.quantity);
     return (
         <Card>
             <CardMedia image = {item.image.url} alt ={item.name} className = {classes.media} />
@@ -18,9 +20,9 @@ const CartItem = ({item}) => {
             </CardContent>
             <CardActions className = {classes.cardActions}>
                 <div className = {classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={() => {if(count!==0) setCount(count - 1)}}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button>
+                    <Button type="button" size="small" onClick={() => setCount(count + 1)}>+</Button>
                 </div>
                 <Button variant="contained" type="button" style = {{backgroundColor: "#EC8484"}} color='primary'>Remove</Button>
             </CardActions>
