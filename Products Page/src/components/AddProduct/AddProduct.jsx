17eb586723bf78332ee97e2b158d46def1eb5271
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-12-07 16:53:16 +0200

    change add product form using react-hook-form

--- Products Page/src/components/AddProduct/AddProduct.jsx
+++ Products Page/src/components/AddProduct/AddProduct.jsx
@@ -1,86 +1,58 @@
-import React, { useState } from 'react';
-import Grid from "@material-ui/core/Grid";
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm, Controller } from "react-hook-form";
+import { Checkbox, Input, Typography, Grid } from "@material-ui/core";
+import { Input as AntdInput, InputNumber } from "antd";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import FlatButton from "@material-ui/core/Button";
-import useStyles from './styles';
-import {PhotoCamera} from '@material-ui/icons';
-import {Typography} from '@material-ui/core';
 import LinkedCameraIcon from '@mui/icons-material/LinkedCamera';
-import { useForm } from "react-hook-form";
+import "./styles.css";
 
 
-const defaultValues = {
-    productName: "",
-    Price: 0,
-    Description: "",
-    Picture: "",
-  };
 
 const AddProduct = () => {
     
-    const classes = useStyles();
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { control, handleSubmit } = useForm();
 
+    const onSubmit = (data) => {
+      alert(JSON.stringify(data));
+    };
 
     return(
-        <form onSubmit={handleSubmit(onSubmit)} className = {classes.root} spacing = {3} style={{ minHeight: '100vh', minWidth: "50%" }}>
-        <Grid container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            alignItems={'center'} 
-            style={{ minHeight: '100vh' }}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Product Image</label>
+      <Controller
+        render={({ field }) => <input type="file" ref={control} />}
+        name="productImage"
+        control={control}
+        defaultValue=""
+      />
+      <label>Product Name</label>
+      <Controller
+        render={({ field }) => <AntdInput {...field} />}
+        name="productName"
+        control={control}
+        defaultValue=""
+      />
+      <label>Product Description</label>
+      <Controller
+        render={({ field }) => <AntdInput {...field} />}
+        name="productDescription"
+        control={control}
+        defaultValue=""
+      />
 
-        <Grid item
-            align = "center"
-            justifyContent="center">
-            <LinkedCameraIcon fontSize="large"  />
-            <Typography variant="h6" align="center">Upload Product Photo</Typography>
-        <FlatButton label="Choose file" labelPosition="before" align="center" >
-        <input type="file"/>
-        </FlatButton>
+      <label>Product Price</label>
+      <Controller
+        render={({ field }) => <InputNumber {...field} min={1} />}
+        name="productPrice"
+        control={control}
+        defaultValue=""
+      />
 
-        </Grid>
-
-        <Grid item>
-        <TextField
-            id="name-input"
-            name="productName"
-            label="Product Name"
-            type="text"
-            value={defaultValues.productName}
-            />
-            </Grid>
-
-            <Grid item>
-            <TextField
-            id="description-input"
-            name="Description"
-            label="Product Description"
-            type="text"
-            value={defaultValues.Description}
-            />
-            </Grid>
-
-            <Grid item>
-            <TextField 
-            id="price-input"
-            name="Price"
-            label="Product Price"
-            type="number"
-            value={defaultValues.Price}
-          />
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary" type="submit" style = {{backgroundColor: "#EC8484"}}>
-            Save
-            </Button>
-            </Grid>
-            </Grid>
-            </form>
+      <input type="submit" />
+    </form>
  
     );
 }
